08310ebf33abe5706261e295bfffb97c91af2157
Kogan, Vladimir <vladimir_kogan@bmc.com>
2019-04-01 14:10:12 +0300

    pre stop hook handler

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-
 // use the express framework
 var express = require('express');
 var app = express();
 const http = require('http');
+const random_name = require('node-random-name');
+var server_name = '';
 
 var test_url;// = process.env.TEST_NODE_URL;
 
@@ -31,18 +32,42 @@ app.get('/', function (req, res) {
   res.send(`${message}`);
 });
 
+app.get('/start', function (req, res) {
+  res.set({
+    'Content-Type': 'text/plain'
+  })
+  let timeNow = (new Date()).toTimeString();
+  setTimeout(() => {
+    sendMesageToTest(`${server_name} POST_START: time: ${timeNow}`);
+  }, 500);
+
+  res.send(`post start`);
+});
+
+app.get('/close', function (req, res) {
+  res.set({
+    'Content-Type': 'text/plain'
+  })
+  let timeNow = (new Date()).toTimeString();
+  sendMesageToTest(`${server_name} START ClEANING PRE_CLOSE START: time: ${timeNow}`);
+  setTimeout(() => {
+    let timeNow = (new Date()).toTimeString();
+    sendMesageToTest(`${server_name} END CLLEANING PRE_CLOSE END: time: ${timeNow}`);
+    res.send(`pre close`);
+    process.exit(0);
+  }, 10000);
+});
+
 // health route - variable subst is more pythonic just as an example
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   var port = server.address().port;
+  setTimeout(() => {
+    sendMesageToTest(`${server_name} LISTENING on port ${port} `);
+  }, 2000);
   console.log('Example app listening on port %s!', port);
 });
 
-
-const data = JSON.stringify({
-  todo: 'Buy the milk'
-})
-
-var sendMesageToTest = msg => {
+function sendMesageToTest(msg) {
   const data = JSON.stringify({
     msg: msg
   });
@@ -63,15 +88,20 @@ var sendMesageToTest = msg => {
   })
   req.on('error', (error) => {
     console.error(error)
-  })
+  });
 
   req.write(data)
   req.end()
 }
 
 process.on('SIGTERM', function () {
+  let timeNow = (new Date()).toTimeString();
+  sendMesageToTest(`${server_name} Got SIGTERM - time: ${timeNow}`);
+
   server.close(function () {
-    process.exit(0);
+    let timeNow = (new Date()).toTimeString();
+    sendMesageToTest(`${server_name} Closed time: ${timeNow}`);
+    //process.exit(0);
   });
 });
 
@@ -80,9 +110,15 @@ setTimeout(() => {
   setInterval(() => {
     let timeNow = (new Date()).toTimeString();
     http.get(`http://` + test_url + `/vladi`);
-    sendMesageToTest(`regular tick: time: ${timeNow} url is :${process.env.TEST_NODE_URL}`);
+    sendMesageToTest(`${server_name} life check: time: ${timeNow}`);
   }, 3000);
-}, 30000);
+}, 200);
+
+server_name = random_name();
+let start_time = (new Date()).toTimeString();
+setTimeout(() => {
+  sendMesageToTest(`${server_name} Started time: ${start_time}`);
+}, 1000);
 
 // export the server to make tests work
 module.exports = server;
